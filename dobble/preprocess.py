624472b4f3b0c7aa488f69e0f0ff7e5702a3b5a8
Thomas Rouch <thom01.rouch@gmail.com>
2024-05-29 10:40:27 +0200

    :sparkles: handle alpha channel

--- dobble/preprocess.py
+++ dobble/preprocess.py
@@ -2,12 +2,14 @@
 """Make all images square and rotation-proof"""
 
 
+import matplotlib.pyplot as plt
 import math
 import os
 
 import cv2
 import numpy as np
 from tqdm import tqdm
+from typing import List
 
 from dobble.utils import new_folder
 
@@ -31,6 +33,33 @@ def _to_square(img: np.ndarray) -> np.ndarray:
     return square
 
 
+def _set_white_background(img: np.ndarray) -> np.ndarray:
+    """Handle alpha channel and set white background."""
+    if len(img.shape) == 2:
+        return cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+    if img.shape[-1] == 3:
+        return img
+
+    assert img.shape[-1] == 4
+
+    alpha = img[..., -1][..., None] / 255.0
+    img = img[..., :3]
+
+    img = (1.0 - alpha) * 255.0 + alpha*img
+    img = img.astype(np.uint8)
+
+    return img
+
+
+def list_image_files(images_folder: str) -> List[str]:
+    """List image files."""
+    image_extensions = ('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff')
+    return [f.name
+            for f in os.scandir(images_folder)
+            if f.name.lower().endswith(image_extensions)]
+
+
 def main(images_folder: str,
          out_images_folder: str):
     """
@@ -41,7 +70,7 @@ def main(images_folder: str,
         images_folder: Folder containing colored images to preprocess
         out_images_folder: Output folder containing the square preprocessed images
     """
-    names = [f.name for f in os.scandir(images_folder)]
+    names = list_image_files(images_folder)
 
     new_folder(out_images_folder)
 
@@ -49,6 +78,7 @@ def main(images_folder: str,
         input_path = os.path.join(images_folder, name)
         output_path = os.path.join(out_images_folder, name)
 
-        img = cv2.imread(input_path)
+        img = cv2.imread(input_path, cv2.IMREAD_UNCHANGED)
+        img = _set_white_background(img)
         img = _to_square(img)
         cv2.imwrite(output_path, img)
